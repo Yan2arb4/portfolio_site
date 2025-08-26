007f42e71fea7e2a8cbe93e5530f078bad0e826f
Yan2arb4 <yan2arb6@hotmail.com>
2025-08-26 18:16:47 -0400

    progress : display multiple projects using project-card component
    Updated the projects component to iterate over a list of project objects and render a project-card for each. Added sample project data to the ProjectsComponent and adjusted styles for layout. The project-card component now serves as a container for individual project details.

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { projectCard } from 'src/app/models/projectCard';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-
+  projects: projectCard[] = [
+    new projectCard(
+      'Discord Bot',
+      true,
+      'A discord bot that provides various functionalities such as minecraft server management, various commands calling APIs, and more.',
+      ['JavaScript', 'MySQL', 'Node.js', 'Discord.js', 'Ubuntu'],
+      'no_link_for_now',
+      'https://placebear.com/500/500'
+    ),
+    new projectCard(
+      'Hearthstone like Card Game',
+      false,
+      'A digital card game inspired by Hearthstone, featuring strategic gameplay, deck building, currency system, and more.',
+      ['Angular', 'TypeScript', 'Asp.Net Core', 'C#', 'Entity Framework', 'SQL Server', 'Azure DevOps'],
+      'no_link_for_now',
+      'https://placebear.com/500/500'
+    ),
+    new projectCard(
+      'Portfolio Website',
+      true,
+      'A personal portfolio website to showcase my skills and projects, built with Angular and hosted on GitHub Pages.',
+      ['Angular', 'TypeScript', 'SCSS', 'HTML5'],
+      'balls',
+      'https://placebear.com/500/500'
+    )
+  ]
 }
