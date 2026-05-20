06ee52d402d983d95776d26453ca1eb295184d08
Yan2arb4 <yan2arb6@hotmail.com>
2026-05-20 02:35:25 -0400

    components: about-me: update styling and html
    Provide new link to updated CV and update overall presentation to be responsive nicely, add about me picture instead of random computer slop I found.

--- portfolio/src/app/components/experience/experience.component.ts
+++ portfolio/src/app/components/experience/experience.component.ts
@@ -22,7 +22,7 @@ export class ExperienceComponent {
         'Created and implemented multiple tests covering more than 80% of custom drivers.',
         'Automated multi-configuration builds to test simultaneous instantiation of multiple drivers to detect conflicts.'
       ],
-      getTechByNames(['Visual Studio Code', 'C Programming Language', 'STM32 Nucleo Series', 'Electronics', 'Zephyr RTOS', 'ZTest Framework', 'Bash', 'Ubuntu', 'Scrum Methodology', 'Git']),
+      getTechByNames(['Visual Studio Code', 'C', 'STM32 Nucleo Series', 'Electronics', 'Zephyr RTOS', 'ZTest Framework', 'Bash', 'Ubuntu', 'Scrum Methodology', 'Git']),
       'assets/images/distech_logo_small.png',
       'assets/images/internship_distech.jpg',
       false
